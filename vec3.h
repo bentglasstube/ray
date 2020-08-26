250016864ba751505fae97bb0a7ffc922b284694
Alan Berndt <alan@eatabrick.org>
2020-08-25 20:19:05 -0700

    Implement diffuse materials.

--- vec3.h
+++ vec3.h
@@ -3,6 +3,8 @@
 #include <cmath>
 #include <iostream>
 
+#include "util.h"
+
 class vec3 {
   public:
     vec3() : e_{0, 0, 0} {}
@@ -40,6 +42,16 @@ class vec3 {
 
     double length_squared() const {
       return e_[0] * e_[0] + e_[1] * e_[1] + e_[2] * e_[2];
+    }
+
+    inline static vec3 random() {
+      return vec3(random_double(), random_double(), random_double());
+    }
+
+    inline static vec3 random(double min, double max) {
+      return vec3(random_double(min, max),
+                  random_double(min, max),
+                  random_double(min, max));
     }
 
   private:
@@ -89,4 +101,11 @@ inline vec3 cross(const vec3 &u, const vec3 &v) {
 
 inline vec3 unit_vector(vec3 v) {
   return v / v.length();
+}
+
+vec3 random_in_unit_sphere() {
+  while (true) {
+    const auto p = vec3::random(-1, 1);
+    if (p.length_squared() < 1) return p;
+  }
 }
